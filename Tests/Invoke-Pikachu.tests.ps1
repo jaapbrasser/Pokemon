f325d49c93488673aa46cb8cdcf810d29762d99c
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-22 13:06:27 -0600

    Suppress output

--- Tests/Invoke-Pikachu.tests.ps1
+++ Tests/Invoke-Pikachu.tests.ps1
@@ -1,6 +1,6 @@
 Describe -Name 'Invoke-Pikachu Function' {
     It -Name 'Function should load' -Test {
         . ../Invoke-Pikachu.ps1
-        Get-Item function:Invoke-PIkachu | Should -Not -BeNullOrEmpty
+        ($null = Get-Item function:Invoke-Pikachu) | Should -Not -BeNullOrEmpty
     }
 }
