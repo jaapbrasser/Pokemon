273bf7ebce06f32a00cf8a5ef0bf492832028b28
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-24 06:27:13 -0600

    Updated function path for GH Actions

--- Tests/Invoke-Pikachu.tests.ps1
+++ Tests/Invoke-Pikachu.tests.ps1
@@ -1,6 +1,6 @@
 Describe -Name 'Invoke-Pikachu Function' {
     It -Name 'Function should load' -Test {
-        . ../Invoke-Pikachu.ps1
+        . /home/runner/work/Pokemon/Pokemon/Invoke-Pikachu.ps1
         ($null = Get-Item function:Invoke-Pikachu) | Should -Not -BeNullOrEmpty
     }
 }
