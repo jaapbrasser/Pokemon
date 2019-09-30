c78d47fe610e835ed1c89061b40328c1c32c03eb
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-30 17:13:33 -0600

    Fixed path

--- Tests/Invoke-Pikachu.tests.ps1
+++ Tests/Invoke-Pikachu.tests.ps1
@@ -1,6 +1,6 @@
 Describe -Name 'Invoke-Pikachu Function' {
     It -Name 'Function should load' -Test {
-        $CorrectPath = Join-Path "$(split-path (Get-Location).path)" 'Invoke-Pikachu.ps1'
+        $CorrectPath = Join-Path "$((Get-Location).path)" 'Invoke-Pikachu.ps1'
         $functioncontent = [io.file]::readalltext($CorrectPath) -replace 'Invoke-Pikachu$'
         $ExecutionContext.InvokeCommand.InvokeScript($functioncontent, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
         ($null = Get-Item function:Invoke-Pikachu) | Should -Not -BeNullOrEmpty
