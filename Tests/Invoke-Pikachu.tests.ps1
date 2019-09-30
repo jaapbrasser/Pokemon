5b7684fea4e22c014752f6b30b12c3628befac64
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-30 17:04:41 -0600

    Build path with Split-Path and Join-Path

--- Tests/Invoke-Pikachu.tests.ps1
+++ Tests/Invoke-Pikachu.tests.ps1
@@ -1,6 +1,7 @@
 Describe -Name 'Invoke-Pikachu Function' {
     It -Name 'Function should load' -Test {
-        $functioncontent = [io.file]::readalltext('../Invoke-Pikachu.ps1') -replace 'Invoke-Pikachu$'
+        $CorrectPath = Join-Path "$(split-path (Get-Location).path)" 'Invoke-Pikachu.ps1'
+        $functioncontent = [io.file]::readalltext($CorrectPath) -replace 'Invoke-Pikachu$'
         $ExecutionContext.InvokeCommand.InvokeScript($functioncontent, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
         ($null = Get-Item function:Invoke-Pikachu) | Should -Not -BeNullOrEmpty
     }
