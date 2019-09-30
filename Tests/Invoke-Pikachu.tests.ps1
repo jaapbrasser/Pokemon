aa9b6d64cc33c4f9d4ce06f5f28379fd13cf95c6
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-30 04:16:46 -0600

    Relative path for cross-platform testing

--- Tests/Invoke-Pikachu.tests.ps1
+++ Tests/Invoke-Pikachu.tests.ps1
@@ -1,6 +1,6 @@
 Describe -Name 'Invoke-Pikachu Function' {
     It -Name 'Function should load' -Test {
-        $functioncontent = [io.file]::readalltext('/home/runner/work/Pokemon/Pokemon/Invoke-Pikachu.ps1') -replace 'Invoke-Pikachu$'
+        $functioncontent = [io.file]::readalltext('../Invoke-Pikachu.ps1') -replace 'Invoke-Pikachu$'
         $ExecutionContext.InvokeCommand.InvokeScript($functioncontent, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
         ($null = Get-Item function:Invoke-Pikachu) | Should -Not -BeNullOrEmpty
     }
